78beb98fb92a82de8f7fa6c5d7731012cad6327f
李越 <mrli126@126.com>
2021-02-15 16:14:14 +0800

    Feat: 壳工程主页面框架+测试Fragment

---
+++ app/src/main/java/com/md/livingstreamsound/test/Fragment4.kt
@@ -0,0 +1,54 @@
+package com.md.livingstreamsound.test
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.md.livingstreamsound.R
+import kotlinx.android.synthetic.main.frag_test.*
+
+class Fragment4:Fragment() {
+    private val currentF = "Fragment4"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e(TAG,"$currentF onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view= LayoutInflater.from(activity).inflate(R.layout.frag_test,container,false)
+       return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tv.text="4"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG,"$currentF onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG,"$currentF onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG,"$currentF onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG,"$currentF onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG,"$currentF onDestroy")
+    }
+}
--- app/src/main/java/com/md/livingstreamsound/ui/MainActivityKt.kt
+++ app/src/main/java/com/md/livingstreamsound/ui/MainActivityKt.kt
@@ -6,10 +6,27 @@ import android.os.Looper
 import android.view.KeyEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.md.livingstreamsound.R
+import com.md.livingstreamsound.test.Fragment1
+import com.md.livingstreamsound.test.Fragment2
+import com.md.livingstreamsound.test.Fragment3
+import com.md.livingstreamsound.test.Fragment4
+import kotlinx.android.synthetic.main.activity_main_kt.*
 
+/**
+ * @author liyue
+ * created 2021/2/15
+ * desc 作为主页面，内部由ViewPager+FragmentPagerAdapter组成
+ */
 class MainActivityKt:AppCompatActivity(),Runnable {
 
+    private val fragments = ArrayList<Fragment>()
+    private lateinit var mainAdapter: MainAdapter
+
+    //防止关闭Activity后Handler还未执行完造成内存泄露
     companion object{
         @JvmStatic
         private val mHandler= Handler(Looper.getMainLooper())
@@ -19,7 +36,8 @@ class MainActivityKt:AppCompatActivity(),Runnable {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_kt)
+        initView()
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -38,5 +56,34 @@ class MainActivityKt:AppCompatActivity(),Runnable {
     override fun run() {
         isExit=false
     }
+
+    private fun initView(){
+        fragments.add(Fragment1())
+        fragments.add(Fragment2())
+        fragments.add(Fragment3())
+        fragments.add(Fragment4())
+
+        mainAdapter= MainAdapter(supportFragmentManager,fragments)
+        viewPager.adapter=mainAdapter
+
+    }
+
+    class MainAdapter:FragmentPagerAdapter{
+        private var fragmentList: List<Fragment> = ArrayList()
+
+        constructor(fm:FragmentManager,fragmentList: List<Fragment>):super(fm){
+            this.fragmentList=fragmentList
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragmentList[position]
+        }
+
+        override fun getCount(): Int {
+           return fragmentList.size
+        }
+
+    }
+
 
 }
