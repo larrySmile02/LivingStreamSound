76deed65fce05380f75eebd3e94a8e8a915bb34b
李越 <mrli126@126.com>
2021-02-28 15:35:43 +0800

    Feat : 给强哥开发入口

--- app/src/main/java/com/md/livingstreamsound/ui/SplashActivity.kt
+++ app/src/main/java/com/md/livingstreamsound/ui/SplashActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.liulishuo.filedownloader.demo.MainDownPlayActivity
 import com.md.livingstreamsound.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity:AppCompatActivity(),Runnable {
+class SplashActivity:AppCompatActivity(),Runnable ,View.OnClickListener{
 
    companion object{
        @JvmStatic
@@ -17,12 +20,29 @@ class SplashActivity:AppCompatActivity(),Runnable {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        mHandler.postDelayed(this,2000)
+        btnMain.setOnClickListener(this)
+        btnDownload.setOnClickListener(this)
+//        mHandler.postDelayed(this,2000)
     }
 
     override fun run() {
         intent= Intent(this,MainActivityKt::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.btnMain->{
+                val intent = Intent(this,MainActivityKt::class.java)
+                startActivity(intent)
+            }
+
+            R.id.btnDownload->{
+                val intent = Intent(this,MainDownPlayActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
     }
 }
